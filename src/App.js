cfecd8ac0f4b25eafed04ba332aa26b0f8b82605
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2023-02-10 12:51:33 +0100

    css commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 import Cars from "./components/Cars"
 import AddCar from './components/AddCar'
 
@@ -27,38 +28,92 @@ function App() {
       occupied: 'true',
     },
   ])
-  
+
+  //
+  useEffect(() => {
+    const getCars = async () => {
+      const carsFromServer = await fetchCars()
+      setCars(carsFromServer)
+    }
+
+    getCars()
+  }, [])
+
+  //Fetch Cars
+  const fetchCars = async () => {
+    const res = await fetch('http://localhost:3000/cars')
+    const data = await res.json()
+
+    return data
+  }
+
+  //Fetch Car
+  const fetchCar = async (id) => {
+    const res = await fetch(`http://localhost:3000/cars/${id}`)
+    const data = await res.json()
+
+    return data
+  }
 
   //Add Car
-  const addCar = (car) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newCar = { id, ...car}
-    setCars([...cars, newCar])
+  const addCar = async (car) => {
+    const res = await fetch('http://localhost:3000/cars', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/jason'
+      },
+      body: JSON.stringify(car),
+    })
+
+    const data = await res.json()
+
+    setCars([...cars, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newCar = { id, ...car}
+    // setCars([...cars, newCar])
   }
 
   //Delete Car
-  const deleteCar = (id) => {
+  const deleteCar = async (id) => {
     //console.log('delete', id)
+    await fetch(`http://localhost:3000/cars/${id}`, {
+      method: 'DELETE'
+    })
+
     setCars(cars.filter((car) => car.id !== id))
   }
 
   //Toggle occupied
-  const toggleOccupied = (id) => {
-    //console.log(id)
+  const toggleOccupied = async (id) => {
+    const carToToggle = await fetchCar(id)
+    const updateCar = { ...carToToggle, occupied: !carToToggle.occupied }
+
+    const res = await fetch(`http://localhost:3000/cars/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updateCar)
+    })
+
+    const data = await res.json()
+
     setCars(
       cars.map((car) => car.id === id ?
-        { ...car, occupied: !car.occupied } : car)
+        { ...car, occupied: data.occupied } : car)
     )
   }
 
 
   return (
     <div className="container">
-      <Header title="Car Share"  onAdd={() => setShowAddCar (!showAddCar)} showAdd={showAddCar}/>
-      {showAddCar && <AddCar onAdd={addCar}/>}
+      <Header title="Car Share" onAdd={() => setShowAddCar(!showAddCar)} showAdd={showAddCar} />
+      {showAddCar && <AddCar onAdd={addCar} />}
       <h2>Good for environment!</h2>
       {cars.length > 0 ? <Cars cars={cars} onDelete={deleteCar}
         onToggle={toggleOccupied} /> : 'No Cars to find'}
+      <Footer />
     </div>
   );
 }
